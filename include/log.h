509a365a860412aeb0403744cbf5ab3f5a130b36
misiek1999 <michaldydo99@gmail.com>
2025-06-19 21:21:45 +0200

    Fix logging lib wrapper for arduino target

--- include/log.h
+++ include/log.h
@@ -28,9 +28,9 @@ inline void chengeLogSource(const LogSource &log_source) {
         case LogSource::UsbSerial:
             Log.begin(LOG_LEVEL_NOTICE, &Serial);
             break;
-        case LogSource::Serial1:
-            Log.begin(LOG_LEVEL_NOTICE, &Serial1);
-            break;
+        // case LogSource::Serial1:
+        //     Log.begin(LOG_LEVEL_NOTICE, &Serial1);
+        //     break;
         // TODO: Implement this function
         // case LogSource::UsbAndSerial1:
         //     break;
@@ -40,27 +40,29 @@ inline void chengeLogSource(const LogSource &log_source) {
     }
 }
 
-inline size_t log_number = {};
+unsigned long get_log_count() {
+    static unsigned long log_number = 0;
+    return log_number++;
+}
 
 // logCount [timestamp[ms]] filename:line_number - message
-#define LOG_PREFIX "%u [%d] %s:%d- "
+#define LOG_PREFIX "%l [%l] %s:%d- "
 #define LOG_MS millis()
 #ifndef __FILENAME__
-constexpr const char* file_name(const char* path) {
-    const char* file = path;
-    while (*path) {
-        if (*path++ == '\\') {
-            file = path;
-        }
-    }
-    return file;
-}
-#define __FILENAME__ file_name(__FILE__)
+#define __FILENAME__ (                                      \
+    __builtin_strrchr(__FILE__, '/')                         \
+      ? __builtin_strrchr(__FILE__, '/') + 1                 \
+      : (                                                    \
+          __builtin_strrchr(__FILE__, '\\')                  \
+            ? __builtin_strrchr(__FILE__, '\\') + 1          \
+            : __FILE__                                       \
+        )                                                    \
+)
 #endif
 
-#define LOG_ERROR(MSG, ...) Log.errorln(LOG_PREFIX MSG, log_number++, LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
-#define LOG_WARNING(MSG, ...) Log.warningln(LOG_PREFIX MSG, log_number++, LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
-#define LOG_NOTICE(MSG, ...) Log.noticeln(LOG_PREFIX MSG, log_number++, LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
-#define LOG_INFO(MSG, ...) Log.infoln(LOG_PREFIX MSG, log_number++, LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
-#define LOG_DEBUG(MSG, ...) Log.traceln(LOG_PREFIX MSG, log_number++, LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
-#define LOG_VERBOSE(MSG, ...) Log.verboseln(LOG_PREFIX MSG, log_number++, LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
+#define LOG_ERROR(MSG, ...) Log.errorln(LOG_PREFIX MSG, get_log_count(), LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
+#define LOG_WARNING(MSG, ...) Log.warningln(LOG_PREFIX MSG, get_log_count(), LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
+#define LOG_NOTICE(MSG, ...) Log.noticeln(LOG_PREFIX MSG, get_log_count(), LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
+#define LOG_INFO(MSG, ...) Log.infoln(LOG_PREFIX MSG, get_log_count(), LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
+#define LOG_DEBUG(MSG, ...) Log.traceln(LOG_PREFIX MSG, get_log_count(), LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
+#define LOG_VERBOSE(MSG, ...) Log.verboseln(LOG_PREFIX MSG, get_log_count(), LOG_MS, __FILENAME__, __LINE__, ##__VA_ARGS__)
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,71 @@
 #include <Arduino.h>    // Essential Arduino header
-#include <LiquidCrystal.h>
+#include "log.h"       // Logging library
 
-// LCD Pin Configuration (Standard for Keypad Shields)
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);  // RS, EN, D4, D5, D6, D7
-// LiquidCrystal lcd(7, 8,  9, 10, 11, 12);  // RS, EN, D4, D5, D6, D7
-
-
-// Backlight Control (Typically pin 10)
-const int BACKLIGHT_PIN = 10; 
-
-// Button Analog Pin
-const int BUTTON_PIN = A0;
 
 void setup() {
-  // Initialize backlight control
-  pinMode(BACKLIGHT_PIN, OUTPUT);
-  digitalWrite(BACKLIGHT_PIN, HIGH);  // Start with backlight ON
-  
-  // Initialize LCD
-  lcd.begin(16, 2);                   // Set dimensions (16x2)
-  lcd.setCursor(0,0);
-  lcd.print("Display Active!");        // Test message
-  delay(1000);                         // Show initial message 
+    // Initialize Serial for logging
+    Serial.begin(115200);
+    while (!Serial);  // Wait for Serial to be ready
+    initLog();        // Initialize the logging system
 }
 
 void loop() {
-  // Turn off backlight after 5 seconds of inactivity
-  static unsigned long lastActive = millis();
-  
-  if (analogRead(BUTTON_PIN) < 1000) {  // If any button pressed
-    lastActive = millis();
-    digitalWrite(BACKLIGHT_PIN, HIGH);  // Ensure backlight ON
-    lcd.clear();
-    lcd.print("Button pressed!");
-    delay(500);
-  }
-  
-  // Auto-backlight off after 5s
-  if (millis() - lastActive > 5000) {
-    digitalWrite(BACKLIGHT_PIN, LOW);
-    lcd.clear();
-    // lcd.noDisplay();  // Additional power saving
-  }
+    // Example log messages
+    LOG_VERBOSE("Verbose log message");
+
+    // Simulate some operations
+    for (int i = 0; i < 5; ++i) {
+        LOG_NOTICE("Iteration %d", i);
+        delay(1000);  // Wait for a second
+    }
+
+    LOG_WARNING("Loop completed, entering sleep mode");
+
+    // Simulate sleep mode
+    delay(5000);  // Sleep for 5 seconds
 }
+
+// #include <LiquidCrystal.h>
+
+// // LCD Pin Configuration (Standard for Keypad Shields)
+// LiquidCrystal lcd(8, 9, 4, 5, 6, 7);  // RS, EN, D4, D5, D6, D7
+// // LiquidCrystal lcd(7, 8,  9, 10, 11, 12);  // RS, EN, D4, D5, D6, D7
+
+
+// // Backlight Control (Typically pin 10)
+// const int BACKLIGHT_PIN = 10;
+
+// // Button Analog Pin
+// const int BUTTON_PIN = A0;
+
+// void setup() {
+//   // Initialize backlight control
+//   pinMode(BACKLIGHT_PIN, OUTPUT);
+//   digitalWrite(BACKLIGHT_PIN, HIGH);  // Start with backlight ON
+
+//   // Initialize LCD
+//   lcd.begin(16, 2);                   // Set dimensions (16x2)
+//   lcd.setCursor(0,0);
+//   lcd.print("Display Active!");        // Test message
+//   delay(1000);                         // Show initial message
+// }
+
+// void loop() {
+//   // Turn off backlight after 5 seconds of inactivity
+//   static unsigned long lastActive = millis();
+
+//   if (analogRead(BUTTON_PIN) < 1000) {  // If any button pressed
+//     lastActive = millis();
+//     digitalWrite(BACKLIGHT_PIN, HIGH);  // Ensure backlight ON
+//     lcd.clear();
+//     lcd.print("Button pressed!");
+//     delay(500);
+//   }
+
+//   // Auto-backlight off after 5s
+//   if (millis() - lastActive > 5000) {
+//     digitalWrite(BACKLIGHT_PIN, LOW);
+//     lcd.clear();
+//     // lcd.noDisplay();  // Additional power saving
+//   }
+// }
