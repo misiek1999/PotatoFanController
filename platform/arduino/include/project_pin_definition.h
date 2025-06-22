b00ff2df495b8a269fcf022495cf4db9fa048c00
misiek1999 <michaldydo99@gmail.com>
2025-06-22 20:24:00 +0200

    Add relay control pin

--- platform/arduino/include/project_pin_definition.h
+++ platform/arduino/include/project_pin_definition.h
@@ -23,3 +23,6 @@ constexpr auto SELECT_BUTTON_PIN    = A2;   // Select button pin
 constexpr auto NEXT_BUTTON_PIN      = A3;   // Next button pin
 constexpr auto INCREASE_BUTTON_PIN  = A4;   // Increase button pin
 constexpr auto DECREASE_BUTTON_PIN  = A5;   // Decrease button pin
+
+// Relay control
+constexpr auto RELAY_PIN            = 12U;  // Pin for relay control
