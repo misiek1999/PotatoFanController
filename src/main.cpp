6011a3a7fdf9c0cbf8e5dd3d28369a3472cbda42
misiek1999 <michaldydo99@gmail.com>
2025-08-05 23:51:09 +0200

    Initialize LCD with Polish characters support in setup

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,7 @@ void setup() {
     internal_sensor.begin();
 
     // Initialize the LCD
+    lcd.beginPolish(16, 2); // Initialize LCD with Polish characters support
     lcd.begin(16, 2);                   // Set dimensions (16x2)
     lcd.setCursor(0,0);
     lcd.print("Aktywacja!");        // Test message
